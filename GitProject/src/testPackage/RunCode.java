58fa7d648bd69e7fc4208fab56c31cb50cfa8452
sumit.mishra48@gmail.com <sumit.mishra@iris-csg-1824.irissoftware.com>
2019-12-24 14:52:50 +0530

    Added File in  myDevBranch

--- GitProject/src/testPackage/RunCode.java
+++ GitProject/src/testPackage/RunCode.java
@@ -5,6 +5,8 @@ public class RunCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Log File Added");
+        System.out.println("Add code");
+        
 	}
 
 }
---
+++ GitProject/src/testPackage/TestCodePage.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class TestCodePage {
+
+	 public void Test()
+	 {
+		 System.out.println("New file added");
+	      System.out.println("New Code File added");      
+	 }
+}
